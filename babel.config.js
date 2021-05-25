528b9c0250f66e9642220041f13ad368cf0a114d
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2021-05-25 20:50:10 -0300

    Preimeiro commit, com apenas um cubo girando

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-module-resolver',
+        {
+          alias: {
+            'expo-three': './node_modules/expo-three/build',
+          },
+        },
+      ],
+    ],
   };
 };
